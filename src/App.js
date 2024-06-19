4bd9b1e3e2a695d2a66504de757da6a285b211c7
Stefan123 <cizmicstefan731@gmail.comm>
2024-06-19 09:59:03 +0200

    Routes Set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
+import React from "react";
+import { Home } from "./Components/Home/Home";
+import { Shop } from "./Components/Shop/Shop";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      console.log("Hello!")
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
