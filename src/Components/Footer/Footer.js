b3347f8f8f21f8df5a8408c818536c13a2841bb1
Stefan123 <cizmicstefan731@gmail.comm>
2024-07-01 17:02:22 +0200

    Footer.js finished

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
 
 export const Footer = () => {
-    return (
-        <div className='footer'></div>
-    )
-}
+  const openSite = (url) => {
+    window.open(url, "_blank", "noreferrer");
+  };
+
+  return (
+    <footer className="footer">
+      <div className="footerContent">
+        <ul>
+          <li onClick={() => openSite("https://www.facebook.com/")}>
+            <FontAwesomeIcon icon={faFacebook} size="lg"/>
+          </li>
+          <li onClick={() => openSite("https://www.instagram.com/")}>
+            <FontAwesomeIcon icon={faInstagram} size="lg"/>
+          </li>
+          <li onClick={() => openSite("https://www.twitter.com/")}>
+            <FontAwesomeIcon icon={faTwitter} size="lg"/>
+          </li>
+        </ul>
+      </div>
+      <div className="footerContent">
+        <ul>
+          <li>About Us</li>
+          <li>FAQs</li>
+          <li>Contact Us</li>
+          <li>Shipping Policy</li>
+          <li>Terms of Service</li>
+        </ul>
+      </div>
+      <div className="footerContent">
+        <input type="text" placeholder="Enter your email"></input>
+        <button>Subscribe</button>
+      </div>
+    </footer>
+  );
+};
