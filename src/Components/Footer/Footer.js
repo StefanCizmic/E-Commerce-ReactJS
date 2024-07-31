874710c3e2b38644835bddf549d1a08cfeac691a
Stefan123 <cizmicstefan731@gmail.comm>
2024-07-31 09:51:42 +0200

    Footer.js modified

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,44 +1,20 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebook,
-  faInstagram,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+import { Social } from "./Social/Social";
+import { Info } from "./Info/Info";
+import { Subscribe } from "./Subscribe/Subscribe";
 import "./Footer.css";
 
 export const Footer = () => {
-  const openSite = (url) => {
-    window.open(url, "_blank", "noreferrer");
-  };
-
   return (
     <footer className="footer">
       <div className="footerContent">
-        <ul>
-          <li onClick={() => openSite("https://www.facebook.com/")}>
-            <FontAwesomeIcon icon={faFacebook} size="lg"/>
-          </li>
-          <li onClick={() => openSite("https://www.instagram.com/")}>
-            <FontAwesomeIcon icon={faInstagram} size="lg"/>
-          </li>
-          <li onClick={() => openSite("https://www.twitter.com/")}>
-            <FontAwesomeIcon icon={faTwitter} size="lg"/>
-          </li>
-        </ul>
+        <Social />
       </div>
       <div className="footerContent">
-        <ul>
-          <li>About Us</li>
-          <li>FAQs</li>
-          <li>Contact Us</li>
-          <li>Shipping Policy</li>
-          <li>Terms of Service</li>
-        </ul>
+        <Info />
       </div>
       <div className="footerContent">
-        <input type="text" placeholder="Enter your email"></input>
-        <button>Subscribe</button>
+        <Subscribe />
       </div>
     </footer>
   );
---
+++ src/Components/Footer/Info/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Info.css";
+
+export const Info = () => {
+  return (
+    <ul>
+      <li>About Us</li>
+      <li>FAQs</li>
+      <li>Contact Us</li>
+      <li>Terms of Service</li>
+    </ul>
+  );
+};
